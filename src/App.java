229a974b3b74e176599fcffdae529af59fde6388
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-10-17 18:57:36 +0300

    Rahojen lisäys käyttäjäsyötteellä
    - Käyttäjä voi lisätä haluamansa määrän rahaa ohjelman alussa.

--- src/App.java
+++ src/App.java
@@ -1,44 +1,69 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		Random arpoja = new Random();
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Syötä rahaa pelataksesi. Kierros maksaa yhden euron");
+		
+		String kayttajaSyote = in.nextLine();
 		int numero = 0;
 		boolean voitto = false;
-		int pelaajanRahat = 1;
+		int pelaajanRahat = Integer.parseInt(kayttajaSyote);
 		int seiskat = 0;
-
-		for (int i = 0; i < 3; i++) {	
-			numero = arpoja.nextInt(1,11);
-			System.out.print("|"+numero+"| ");
-			if (numero == 7) {
-				voitto = true;
-				seiskat += 1;
+		
+		do {
+		    pelaajanRahat -= 1;
+		
+		    for (int i = 0; i < 3; i++) {
+			    numero = arpoja.nextInt(1,11);
+					System.out.print("|"+numero+"| ");
+					if (numero == 7) {
+						voitto = true;
+						seiskat += 1;
+					}
+				}
+		
+			if (seiskat == 3) {
+			    pelaajanRahat += 10;
 			}
-			System.out.println("V "+voitto+" "+seiskat);
-		}
-		
-		if (seiskat == 3) {
-			pelaajanRahat += 7;
-		}
-		else if (seiskat == 2) {
-			pelaajanRahat += 5;
-		} 
-		else if (seiskat == 1) {
+            else if (seiskat == 2) {
+				pelaajanRahat += 5;
+			}
+            else if (seiskat == 1) {
 			pelaajanRahat += 2;
-		}
-		else 	
+			}
+			else 	
+					
+			System.out.println("");
+				
+			if (voitto != true) {
+				System.out.println("Hävisit");
+			} else {
+				System.out.println("Voitit");
+			}
+				
+			System.out.println("Sinulla on jäljellä: "+pelaajanRahat+" €");
+			System.out.println("Haluatko pelata uudestaan? Jos haluat pelata paina Enter.\nJos et halua pelata  kirjoita e ja paina Enter ");
+			kayttajaSyote = in.nextLine().toLowerCase();
+			if (kayttajaSyote.equals("e")) {
+				break;
+			} 
+            else {
+				seiskat = 0;
+				continue;
+            }
+            			
+		} while (pelaajanRahat > 0);
 		
-		System.out.println("");
-		System.out.println(voitto);
-		System.out.println("Pelaajan rahat: "+pelaajanRahat);
-		if (voitto != true) {
-			System.out.println("Hävisit");
-		} 
-        else
-			System.out.println("Voitit");
+		if (pelaajanRahat == 0) {
+			System.out.println("Rahasi loppuivat.");
+		} else 
+			System.out.println("Lopetit pelin.");
 	}	
 		
 }
