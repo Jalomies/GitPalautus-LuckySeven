71760fb9b96c8e9ec412537727c894b92c2ba8af
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-10-17 18:48:17 +0300

    Peliin lisätty rahat
    - Rahat lisästään suoraan koodiin

--- src/App.java
+++ src/App.java
@@ -7,21 +7,37 @@ public class App {
 		Random arpoja = new Random();
 		int numero = 0;
 		boolean voitto = false;
-		
-		for (int i = 0; i < 3; i++) {
-			
+		int pelaajanRahat = 1;
+		int seiskat = 0;
+
+		for (int i = 0; i < 3; i++) {	
 			numero = arpoja.nextInt(1,11);
 			System.out.print("|"+numero+"| ");
 			if (numero == 7) {
-			    voitto = true;
-				System.out.println("V "+voitto);
-            }
+				voitto = true;
+				seiskat += 1;
+			}
+			System.out.println("V "+voitto+" "+seiskat);
 		}
+		
+		if (seiskat == 3) {
+			pelaajanRahat += 7;
+		}
+		else if (seiskat == 2) {
+			pelaajanRahat += 5;
+		} 
+		else if (seiskat == 1) {
+			pelaajanRahat += 2;
+		}
+		else 	
+		
 		System.out.println("");
 		System.out.println(voitto);
+		System.out.println("Pelaajan rahat: "+pelaajanRahat);
 		if (voitto != true) {
 			System.out.println("Hävisit");
-		} else
+		} 
+        else
 			System.out.println("Voitit");
 	}	
 		
