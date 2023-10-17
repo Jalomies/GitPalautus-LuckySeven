adf708ea20b361d87ec91e6ee8c24f907c18fc81
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-10-17 19:07:05 +0300

    Valmis peli 1.0
    - Peli tarkistaa että käyttäjän syöte on oikeanlainen eikä kaada ohjelmaa.
    -Voiton suuruus riippuu seiiskojen määrästä.
    -Pelistä on poistettu bugeja jotka mahdollistivat pelaamisen vaikka rahaa on 0.

--- src/App.java
+++ src/App.java
@@ -7,63 +7,81 @@ public class App {
 		
 		Random arpoja = new Random();
 		Scanner in = new Scanner(System.in);
-		
 		System.out.println("Syötä rahaa pelataksesi. Kierros maksaa yhden euron");
 		
-		String kayttajaSyote = in.nextLine();
-		int numero = 0;
-		boolean voitto = false;
+		String kayttajaSyote = "";
+		while(true) {
+		
+		kayttajaSyote = in.nextLine();
+		
+		try {
+			int testi = Integer.parseInt(kayttajaSyote) -1 ;
+			break;
+		} catch (Exception e) {
+			System.out.println("Syötä vain numeroita");
+			continue;
+		}
+		}
+		
 		int pelaajanRahat = Integer.parseInt(kayttajaSyote);
-		int seiskat = 0;
+		int numero = 0;
+		int seiskat = 0;	
+		boolean voitto = false;	
+	
+		while (pelaajanRahat > 0){
 		
-		do {
-		    pelaajanRahat -= 1;
+			pelaajanRahat -= 1;
 		
-		    for (int i = 0; i < 3; i++) {
-			    numero = arpoja.nextInt(1,11);
-					System.out.print("|"+numero+"| ");
-					if (numero == 7) {
-						voitto = true;
-						seiskat += 1;
-					}
+			for (int i = 0; i < 3; i++) {
+				numero = arpoja.nextInt(1,11);
+				System.out.print("|"+numero+"| ");
+				if (numero == 7) {
+					voitto = true;
+					seiskat += 1;
 				}
+			}
 		
 			if (seiskat == 3) {
-			    pelaajanRahat += 10;
+				pelaajanRahat += 10;
 			}
-            else if (seiskat == 2) {
+			else if (seiskat == 2) {
 				pelaajanRahat += 5;
-			}
-            else if (seiskat == 1) {
-			pelaajanRahat += 2;
+			} 
+			else if (seiskat == 1) {
+				pelaajanRahat += 2;
 			}
 			else 	
 					
 			System.out.println("");
 				
 			if (voitto != true) {
-				System.out.println("Hävisit");
-			} else {
-				System.out.println("Voitit");
+				System.out.println("Hävisit!");
+			}
+			else {
+				System.out.println("Voitit! ");
 			}
 				
-			System.out.println("Sinulla on jäljellä: "+pelaajanRahat+" €");
+			System.out.println("Sinulla on jäljellä: "+pelaajanRahat+" euroa");
+			if (pelaajanRahat == 0) {
+				break;
+			}
 			System.out.println("Haluatko pelata uudestaan? Jos haluat pelata paina Enter.\nJos et halua pelata  kirjoita e ja paina Enter ");
 			kayttajaSyote = in.nextLine().toLowerCase();
 			if (kayttajaSyote.equals("e")) {
 				break;
-			} 
-            else {
+			}
+			else
+				voitto = false;
 				seiskat = 0;
-				continue;
-            }
-            			
-		} while (pelaajanRahat > 0);
+					
+		}
 		
 		if (pelaajanRahat == 0) {
 			System.out.println("Rahasi loppuivat.");
-		} else 
+		} 
+		else {
 			System.out.println("Lopetit pelin.");
+		}
 	}	
 		
 }
