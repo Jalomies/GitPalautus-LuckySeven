ce69e29b5ae61f2e89ff73e3f33dff1691bb7912
Jalomies <142224339+Jalomies@users.noreply.github.com>
2023-10-17 18:43:00 +0300

    Pelin runko

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		
+		Random arpoja = new Random();
+		int numero = 0;
+		boolean voitto = false;
+		
+		for (int i = 0; i < 3; i++) {
+			
+			numero = arpoja.nextInt(1,11);
+			System.out.print("|"+numero+"| ");
+			if (numero == 7) {
+			    voitto = true;
+				System.out.println("V "+voitto);
+            }
+		}
+		System.out.println("");
+		System.out.println(voitto);
+		if (voitto != true) {
+			System.out.println("Hävisit");
+		} else
+			System.out.println("Voitit");
+	}	
+		
 }
